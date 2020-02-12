79832cf7f8356b70b4258c0675102994630a614e
Takuya-Shuto-engineer <shuto.takuya.sy@alumni.tsukuba.ac.jp>
2020-02-12 23:35:21 +0900

    Tiny modification of visits count initialization

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -10,8 +10,8 @@ class HelloController @Inject()(cc: ControllerComponents) extends AbstractContro
   // def hello(): Action[AnyContent] = Action(Ok("Hello World"))
   def hello(): Action[AnyContent] = Action{
     request => {
-      val uuid: String = request.cookies.get("userId").map(_.value).getOrElse(java.util.UUID.randomUUID.toString)
-      val count: String = request.cookies.get("count").map(_.value).getOrElse("1")
+      val uuid: String = request.cookies.get("userId").map(_.value).getOrElse(java.util.UUID.randomUUID.toString) // CookieにIDがなければ生成
+      val count: String = request.cookies.get("count").map(_.value).getOrElse("0") // cookieにカウントがない初見さんは0回で初期化
       val currentCount: String = (count.toInt + 1).toString
       val response: String = s"Hello user $uuid" + s"\nYou've been here for $currentCount times"
       Ok(response).withCookies(Cookie("userId", uuid), Cookie("count", currentCount))
@@ -20,8 +20,8 @@ class HelloController @Inject()(cc: ControllerComponents) extends AbstractContro
 
   def helloJson():Action[AnyContent] = Action{
     request => {
-      val uuid: String = request.cookies.get("userId").map(_.value).getOrElse(java.util.UUID.randomUUID.toString)
-      val count: String = request.cookies.get("count").map(_.value).getOrElse("1")
+      val uuid: String = request.cookies.get("userId").map(_.value).getOrElse(java.util.UUID.randomUUID.toString) // hello()と一緒
+      val count: String = request.cookies.get("count").map(_.value).getOrElse("0") // hello()と一緒
       val currentCount: String = (count.toInt + 1).toString
       val json: JsValue = Json.obj("hello" -> s"user$uuid", "language" -> "scala", "count" -> currentCount)
       Ok(json).withCookies(Cookie("userId", uuid), Cookie("count", currentCount))

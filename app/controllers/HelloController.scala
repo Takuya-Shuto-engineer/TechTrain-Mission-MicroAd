ae5b8790c4d1fe5739b078e30b4ec8b60473379b
Takuya-Shuto-engineer <shuto.takuya.sy@alumni.tsukuba.ac.jp>
2020-02-14 18:00:58 +0900

    Added views/hello.scala.html with cookie http response

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -7,24 +7,24 @@ import play.api.mvc._
 
 
 class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  // def hello(): Action[AnyContent] = Action(Ok("Hello World"))
+
   def hello(): Action[AnyContent] = Action{
     request => {
-      val uuid: String = request.cookies.get("userId").map(_.value).getOrElse(java.util.UUID.randomUUID.toString) // CookieにIDがなければ生成
+      val userId: String = request.cookies.get("userId").map(_.value).getOrElse(java.util.UUID.randomUUID.toString) // CookieにIDがなければ生成
       val count: String = request.cookies.get("count").map(_.value).getOrElse("0") // cookieにカウントがない初見さんは0回で初期化
       val currentCount: String = (count.toInt + 1).toString
-      val response: String = s"Hello user $uuid" + s"\nYou've been here for $currentCount times"
-      Ok(response).withCookies(Cookie("userId", uuid), Cookie("count", currentCount))
+      val response: String = s"Hello user $userId" + s"\nYou've been here for $currentCount times"
+      Ok(views.html.hello(response)).withCookies(Cookie("userId", userId), Cookie("count", currentCount)).bakeCookies()
     }
   }
 
   def helloJson():Action[AnyContent] = Action{
     request => {
-      val uuid: String = request.cookies.get("userId").map(_.value).getOrElse(java.util.UUID.randomUUID.toString) // hello()と一緒
+      val userId: String = request.cookies.get("userId").map(_.value).getOrElse(java.util.UUID.randomUUID.toString) // hello()と一緒
       val count: String = request.cookies.get("count").map(_.value).getOrElse("0") // hello()と一緒
       val currentCount: String = (count.toInt + 1).toString
-      val json: JsValue = Json.obj("hello" -> s"user$uuid", "language" -> "scala", "count" -> currentCount)
-      Ok(json).withCookies(Cookie("userId", uuid), Cookie("count", currentCount))
+      val json: JsValue = Json.obj("hello" -> s"user $userId", "language" -> "scala", "count" -> currentCount)
+      Ok(json).withCookies(Cookie("userId", userId), Cookie("count", currentCount)).bakeCookies()
     }
   }
 }
